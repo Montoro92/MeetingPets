7c881fa1c8fc21d42a5f77a377d87c00874096dc
Montoro <vimon_92@hotmail.com>
2018-02-21 21:27:46 +0100

    Here, I've created the first entity of the application. This will change after. Secondly, I've created the properties of the data base connection and the repository to access to it.

---
+++ src/main/java/com/mycompany/meetingpets/Entities/Pet.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.meetingpets.Entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author montoro
+ */
+@Entity
+public class Pet {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="id")
+    private Long id;
+    
+    @Column(name="name")
+    @NotNull
+    private String name;
+    
+    public Pet(){}
+    
+    public Pet(String name) {
+        this.name=name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
